f3e2b1b39e6bf24f51a436e51b863e24a646e3c5
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-04-01 00:16:33 +0600

    Added role and permission value initializer method

--- src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/configs/DbValueInitializer.java
+++ src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/configs/DbValueInitializer.java
@@ -1,29 +1,63 @@
 package com.codecraftershub.telemedicine.configs;
 
+import com.codecraftershub.telemedicine.entities.user.Permission;
 import com.codecraftershub.telemedicine.entities.user.Role;
+import com.codecraftershub.telemedicine.enums.UserPermission;
 import com.codecraftershub.telemedicine.enums.UserRole;
+import com.codecraftershub.telemedicine.repositories.user.PermissionRepository;
 import com.codecraftershub.telemedicine.repositories.user.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Component
 @RequiredArgsConstructor
 public class DbValueInitializer implements ApplicationRunner {
-    private final RoleRepository repository;
+    private final RoleRepository roleRepository;
+    private final PermissionRepository permissionRepository;
+    private Map<String, List<Permission>> permissionMap;
 
     @Override
     public void run(ApplicationArguments args) {
+        permissionMap = new HashMap<>();
+        insertPermissionToDB();
         insertUserRolesToDB();
+    }
+
+    private void insertPermissionToDB() {
+        for (UserPermission permission : UserPermission.values()) {
+            var createdEntity = permissionRepository.save(Permission
+                    .builder()
+                    .key(permission.toString())
+                    .name(permission.getDisplayValue())
+                    .build());
+
+            String key = createdEntity.getKey().split("_")[0];
+
+            List<Permission> permissions;
+            if (!permissionMap.containsKey(key)) {
+                permissions = new ArrayList<>();
+            } else {
+                permissions = permissionMap.get(key);
+            }
+            permissions.add(createdEntity);
+            permissionMap.put(key, permissions);
+        }
     }
 
     private void insertUserRolesToDB() {
         for (UserRole userRole : UserRole.values()) {
-            repository.save(Role
+            roleRepository.save(Role
                     .builder()
                     .key(userRole.toString())
                     .name(userRole.getDisplayValue())
+                    .permissions(permissionMap.get(userRole.toString()))
                     .build());
         }
     }
--- src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/repositories/user/DoctorRepository.java
+++ src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/repositories/user/DoctorRepository.java
@@ -2,6 +2,8 @@ package com.codecraftershub.telemedicine.repositories.user;
 
 import com.codecraftershub.telemedicine.entities.user.Doctor;
 import com.codecraftershub.telemedicine.repositories.BaseRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DoctorRepository extends BaseRepository<Doctor, Long> {
 }
