eebaadc0a308941af4f873e5cbc500bcb3b3d1e3
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-04-01 19:42:39 +0600

    Added admin user creation via DbValueInitializer

--- src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/configs/DbValueInitializer.java
+++ src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/configs/DbValueInitializer.java
@@ -2,32 +2,35 @@ package com.codecraftershub.telemedicine.configs;
 
 import com.codecraftershub.telemedicine.entities.user.Permission;
 import com.codecraftershub.telemedicine.entities.user.Role;
+import com.codecraftershub.telemedicine.entities.user.User;
 import com.codecraftershub.telemedicine.enums.UserPermission;
 import com.codecraftershub.telemedicine.enums.UserRole;
 import com.codecraftershub.telemedicine.repositories.user.PermissionRepository;
 import com.codecraftershub.telemedicine.repositories.user.RoleRepository;
+import com.codecraftershub.telemedicine.repositories.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
 public class DbValueInitializer implements ApplicationRunner {
     private final RoleRepository roleRepository;
     private final PermissionRepository permissionRepository;
+    private final UserRepository userRepository;
     private Map<String, List<Permission>> permissionMap;
+    private Map<String, Role> roleMap;
 
     @Override
     public void run(ApplicationArguments args) {
         permissionMap = new HashMap<>();
+        roleMap = new HashMap<>();
         insertPermissionToDB();
         insertUserRolesToDB();
+        createAdminUser();
     }
 
     private void insertPermissionToDB() {
@@ -53,12 +56,29 @@ public class DbValueInitializer implements ApplicationRunner {
 
     private void insertUserRolesToDB() {
         for (UserRole userRole : UserRole.values()) {
-            roleRepository.save(Role
+            var createdEntity = roleRepository.save(Role
                     .builder()
                     .key(userRole.toString())
                     .name(userRole.getDisplayValue())
                     .permissions(permissionMap.get(userRole.toString()))
                     .build());
+
+            roleMap.put(userRole.toString(), createdEntity);
         }
+    }
+
+    private void createAdminUser() {
+        User user = User
+                .builder()
+                .firstName("Admin")
+                .username("admin")
+                .password("admin")
+                .email("admin@gmail.com")
+                .phoneNo("123456789")
+                .isActive(true)
+                .roles(Collections.singletonList(roleMap.get(UserRole.ADMIN.toString())))
+                .build();
+
+        userRepository.save(user);
     }
 }
