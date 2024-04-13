342292f3922c64d4dc1f3682d57efd8ef264ea8b
Pial Kanti Samadder <pialkanti2012@gmail.com>
2024-04-13 18:59:11 +0600

    Skipped not active specialities from find all endpoint

--- src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/repositories/user/SpecialityRepository.java
+++ src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/repositories/user/SpecialityRepository.java
@@ -2,8 +2,13 @@ package com.codecraftershub.telemedicine.repositories.user;
 
 import com.codecraftershub.telemedicine.entities.user.doctor.Speciality;
 import com.codecraftershub.telemedicine.repositories.BaseRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SpecialityRepository extends BaseRepository<Speciality, Long> {
+    @Query("select s from Speciality s where s.isActive = true")
+    <T> Page<T> findAllActive(Pageable pageable, Class<T> type);
 }
--- src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/services/user/SpecialityService.java
+++ src/Api/telemedicine/src/main/java/com/codecraftershub/telemedicine/services/user/SpecialityService.java
@@ -1,17 +1,35 @@
 package com.codecraftershub.telemedicine.services.user;
 
+import com.codecraftershub.telemedicine.dtos.BasePaginatedResponse;
 import com.codecraftershub.telemedicine.dtos.requests.users.SpecialityCreateRequest;
 import com.codecraftershub.telemedicine.dtos.requests.users.SpecialityUpdateRequest;
 import com.codecraftershub.telemedicine.entities.user.doctor.Speciality;
 import com.codecraftershub.telemedicine.repositories.user.SpecialityRepository;
 import com.codecraftershub.telemedicine.services.BaseService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpecialityService extends BaseService<Speciality, Long, SpecialityCreateRequest, SpecialityUpdateRequest, Speciality> {
+    private final SpecialityRepository repository;
 
     public SpecialityService(SpecialityRepository repository) {
         super(repository);
+        this.repository = repository;
+    }
+
+    @Override
+    public <T> BasePaginatedResponse<T> findAll(Pageable pageable, Class<T> type) {
+        var page = repository.findAllActive(pageable, type);
+
+        return BasePaginatedResponse
+                .<T>builder()
+                .page(page.getNumber())
+                .pageSize(page.getSize())
+                .totalItems(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .data(page.getContent())
+                .build();
     }
 
     @Override
